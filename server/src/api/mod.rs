fc0872741f409ce4e784a2e974e61ae7900fbfa2
Aidan Lynch <aidantlynch00@gmail.com>
2025-01-29 23:57:35 -0500

    Created the structures to parse objects from the public Slapshot API
    Added structures for parsing different IDs and match info. These can be
    used to deserialize JSON responses from slapshot.gg/players/:id and
    slapshot.gg/matches/:id.

---
+++ server/src/api/mod.rs
@@ -0,0 +1,54 @@
+pub mod slapshot;
+pub mod tracker;
+
+use std::sync::Arc;
+use std::ops::Deref;
+use serde::{Serialize, Deserialize};
+
+#[derive(Clone, Serialize, Deserialize)]
+pub struct Username(Arc<str>);
+
+impl From<&str> for Username {
+    fn from(value: &str) -> Self {
+        Username(Arc::from(value))
+    }
+}
+
+impl Deref for Username {
+    type Target = str;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+#[derive(Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct PlayerId(Arc<str>);
+
+impl From<&str> for PlayerId {
+    fn from(value: &str) -> Self {
+        PlayerId(Arc::from(value))
+    }
+}
+
+impl Deref for PlayerId {
+    type Target = str;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+#[derive(Clone, Serialize, Deserialize)]
+pub struct MatchId(Arc<str>);
+
+impl From<&str> for MatchId {
+    fn from(value: &str) -> Self {
+        MatchId(Arc::from(value))
+    }
+}
+
+impl Deref for MatchId {
+    type Target = str;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
---
+++ server/src/api/slapshot.rs
@@ -0,0 +1,81 @@
+use serde::Deserialize;
+use serde::de::Error;
+use time::OffsetDateTime;
+use time::serde::iso8601;
+use crate::api::{Username, PlayerId, MatchId};
+
+#[derive(Deserialize)]
+pub struct RecentHistory {
+    #[serde(flatten)]
+    pub player: Player,
+    pub match_history: Vec<Match>,
+}
+
+#[derive(Deserialize)]
+pub struct Player {
+    pub username: Username,
+    pub game_user_id: PlayerId,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum Mode {
+    Hockey,
+    Pond,
+    Dodgepuck,
+    Tag,
+}
+
+#[derive(Deserialize)]
+pub struct Match {
+    pub id: MatchId,
+    #[serde(with = "iso8601")]
+    pub created: OffsetDateTime,
+    #[serde(rename = "gamemode")]
+    pub mode: Mode,
+    pub game_stats: Option<MatchStats>,
+}
+
+#[derive(Deserialize)]
+pub struct MatchStats {
+    #[serde(deserialize_with = "none_as_option")]
+    pub winner: Option<Team>,
+    pub score: Score,
+    pub players: Vec<MatchPlayer>
+}
+
+#[derive(Deserialize)]
+pub struct Score {
+    pub away: u8,
+    pub home: u8
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum Team {
+    Home,
+    Away
+}
+
+fn none_as_option<'de, D>(deserializer: D) -> Result<Option<Team>, D::Error>
+where D: serde::Deserializer<'de>
+{
+    let slice: &str = Deserialize::deserialize(deserializer)?;
+    match slice {
+        "none" => Ok(None),
+        "home" => Ok(Some(Team::Home)),
+        "away" => Ok(Some(Team::Away)),
+        _ => Err(Error::custom(format!("Invalid team: {}", slice)))
+    }
+}
+
+#[derive(Deserialize)]
+pub struct MatchPlayer {
+    #[serde(flatten)]
+    pub player: Player,
+    pub team: Team,
+    pub stats: PlayerStats
+}
+
+#[derive(Deserialize)]
+pub struct PlayerStats {}

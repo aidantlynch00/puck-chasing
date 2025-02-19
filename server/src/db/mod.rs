65811f38f37f0432bd238a57b2c7ce639f3b5766
Aidan Lynch <aidantlynch00@gmail.com>
2025-02-19 00:01:29 -0500

    Created unit tests for creating a connection pool and getting connections
    Used the pool builder to set the max number of connections. This is
    helpful for the unit tests to be able to take all of the connections.
    Created a helper function for getting a connection. Wrote complete unit
    tests for opening a connection pool, taking an available connection, and
    attempting to take a connection when none are available. Tests for the
    `create_tables` and `add_player` functions are stubbed.

--- server/src/db/mod.rs
+++ server/src/db/mod.rs
@@ -6,9 +6,11 @@ use diesel::{sql_query, SqliteConnection, SelectableHelper};
 use diesel_async::RunQueryDsl;
 use diesel_async::sync_connection_wrapper::SyncConnectionWrapper;
 use diesel_async::pooled_connection::{AsyncDieselConnectionManager, PoolError};
-use diesel_async::pooled_connection::bb8::{Pool, RunError};
+use diesel_async::pooled_connection::bb8::{Pool, PooledConnection, RunError};
 use crate::types::slapshot::Player;
 use crate::types::db::{NewPlayerRow, PlayerRow};
+
+static POOL_MAX_CONNS: usize = 10;
 
 type AsyncSqliteConnection = SyncConnectionWrapper<SqliteConnection>;
 
@@ -21,18 +23,21 @@ impl ConnectionPool {
     pub async fn open<S: Into<String>>(path: S) -> Result<Self, RunError> {
         let manager = AsyncDieselConnectionManager::<AsyncSqliteConnection>::new(path);
         let pool = Pool::builder()
+            .max_size(POOL_MAX_CONNS as u32)
             .build(manager)
             .await?;
 
         Ok(Self { pool })
     }
 
+    pub async fn conn(&self) -> Result<PooledConnection<AsyncSqliteConnection>, RunError> {
+        self.pool.get().await
+    }
+
     pub async fn add_player(&self, player: &Player) -> Result<PlayerRow, RunError> {
         use crate::db::schema::players::dsl::*;
 
-        let mut conn = self.pool
-            .get()
-            .await?;
+        let mut conn = self.conn().await?;
 
         let new_player = NewPlayerRow {
             slap_id: Cow::Borrowed(&player.game_user_id),
@@ -47,9 +52,7 @@ impl ConnectionPool {
     }
 
     pub async fn create_tables(&self) -> Result<(), RunError> {
-        let mut conn = self.pool
-            .get()
-            .await?;
+        let mut conn = self.conn().await?;
 
         let _ = sql_query(r#"
             CREATE TABLE IF NOT EXISTS players (
@@ -100,5 +103,81 @@ impl ConnectionPool {
         // TODO: create ranks table
 
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use diesel_async::pooled_connection::bb8::{PooledConnection, RunError};
+    use super::*;
+
+    async fn open_in_memory() -> Result<ConnectionPool, RunError> {
+        ConnectionPool::open(":memory:").await
+    }
+
+    async fn take_all_conns(pool: &ConnectionPool) -> Vec<PooledConnection<AsyncSqliteConnection>> {
+        let mut conns = Vec::new();
+        for _ in 0..POOL_MAX_CONNS {
+            let conn = pool.conn()
+                .await
+                .unwrap();
+
+            conns.push(conn);
+        }
+
+        conns
+    }
+
+    #[tokio::test]
+    async fn open_pool() {
+        let pool_res = open_in_memory().await;
+        assert!(pool_res.is_ok());
+    }
+
+    #[tokio::test]
+    async fn conn_available() {
+        let pool = open_in_memory()
+            .await
+            .unwrap();
+
+        let conn_res = pool.conn().await;
+        assert!(conn_res.is_ok());
+    }
+
+    #[tokio::test]
+    async fn no_conn_available() {
+        let pool = open_in_memory()
+            .await
+            .unwrap();
+
+        let conns = take_all_conns(&pool).await;
+        let conn_res = pool.conn().await;
+
+        // explicitly hold connections until last connection future returns
+        drop(conns);
+
+        // test last connection result after dropping other connections to
+        // avoid any leaks during unwind
+        assert!(conn_res.is_err());
+    }
+
+    #[tokio::test]
+    async fn create_tables() {
+        todo!();
+    }
+
+    #[tokio::test]
+    async fn tables_exist() {
+        todo!();
+    }
+
+    #[tokio::test]
+    async fn add_player() {
+        todo!();
+    }
+
+    #[tokio::test]
+    async fn player_exists() {
+        todo!();
     }
 }

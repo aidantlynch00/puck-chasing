3e716bbcd972b5f7420343d6233dfaac7b958e7f
Christopher Murano <fruitloopsgo@gmail.com>
2025-03-07 01:21:18 -0500

    Added random messages to error page and cleaned up some code surrounding the layout

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,7 +1,16 @@
 <script>
     let { children } = $props();
+    import { page } from '$app/stores'
+    import bgimg from '$lib/images/pc-bg2.png'
     import "../app.css";
 </script>
+
+{#if $page.url.pathname !== '/'}
+    <div id="background" class="relative w-full h-screen overflow-hidden">
+        <div class="absolute inset-0 bg-black"></div>
+        <div style="background-image: url({bgimg}); filter: blur(8px); transform: scale(1.1);" class="absolute inset-0 bg-cover bg-center opacity-50"></div>
+    </div>
+{/if}
 
 {@render children()}
 
--- client/src/routes/player/[slapid]/+page.svelte
+++ client/src/routes/player/[slapid]/+page.svelte
@@ -2,4 +2,6 @@
     import { page } from '$app/state'
 </script>
 
-<p>This is the player page of the user with slapid: {page.params.slapid}</p>
+<div id="main" class="absolute inset-0 flex justify-center items-center z-10">
+    <p>This is the player page of the user with slapid: {page.params.slapid}</p>
+</div>

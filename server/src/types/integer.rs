134892dba3a6f4af73f34d1a590a3b601888162f
Aidan Lynch <aidantlynch00@gmail.com>
2025-03-11 18:07:57 -0400

    Created a macro to generate integer types
    Similar to the `string_type!` macro, this new `integer_type!` macro
    generates the code for an integer type. This is currently used for the
    internal ID columns for the players and matches tables.

--- server/src/types/integer.rs
+++ server/src/types/integer.rs
@@ -1,63 +1,36 @@
-use diesel::{AsExpression, FromSqlRow};
-use diesel::sql_types::Integer;
-use diesel::backend::Backend;
-use diesel::sqlite::Sqlite;
-use diesel::serialize::{ToSql, Output, Result as SerResult};
-use diesel::deserialize::{FromSql, Result as DeserResult};
+macro_rules! integer_type {
+    ($type:ident) => {
+        #[derive(
+            Debug, Clone, Copy, PartialEq, Eq, Hash,
+            ::diesel::AsExpression, ::diesel::FromSqlRow,
+        )]
+        #[diesel(sql_type = ::diesel::sql_types::Integer)]
+        pub struct $type(i32);
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, AsExpression, FromSqlRow)]
-#[diesel(sql_type = Integer)]
-struct InternalId(i32);
+        impl ::diesel::serialize::ToSql<::diesel::sql_types::Integer, ::diesel::sqlite::Sqlite> for $type {
+            fn to_sql<'b>(
+                &'b self,
+                out: &mut ::diesel::serialize::Output<'b, '_, ::diesel::sqlite::Sqlite>
+            ) -> ::diesel::serialize::Result {
+                <i32 as ::diesel::serialize::ToSql<::diesel::sql_types::Integer, ::diesel::sqlite::Sqlite>>::to_sql(
+                    &self.0,
+                    out
+                )
+            }
+        }
 
-impl ToSql<Integer, Sqlite> for InternalId {
-    fn to_sql<'b>(&'b self, out: &mut Output<'b, '_, Sqlite>) -> SerResult {
-        <i32 as ToSql<Integer, Sqlite>>::to_sql(&self.0, out)
+        impl ::diesel::deserialize::FromSql<::diesel::sql_types::Integer, ::diesel::sqlite::Sqlite> for $type {
+            fn from_sql(
+                bytes: <::diesel::sqlite::Sqlite as ::diesel::backend::Backend>::RawValue<'_>
+            ) -> ::diesel::deserialize::Result<Self> {
+                let id = <i32 as ::diesel::deserialize::FromSql<::diesel::sql_types::Integer, ::diesel::sqlite::Sqlite>>::from_sql(bytes)
+                    .expect("column can be expressed as integer type!");
+
+                Ok($type(id))
+            }
+        }
     }
 }
 
-impl FromSql<Integer, Sqlite> for InternalId {
-    fn from_sql(bytes: <Sqlite as Backend>::RawValue<'_>) -> DeserResult<Self> {
-        let id = <i32 as FromSql<Integer, Sqlite>>::from_sql(bytes)
-            .expect("column can be expressed as integer type!");
-
-        Ok(InternalId(id))
-    }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, AsExpression, FromSqlRow)]
-#[diesel(sql_type = Integer)]
-pub struct InternalPlayerId(InternalId);
-
-impl ToSql<Integer, Sqlite> for InternalPlayerId {
-    fn to_sql<'b>(&'b self, out: &mut Output<'b, '_, Sqlite>) -> SerResult {
-        <InternalId as ToSql<Integer, Sqlite>>::to_sql(&self.0, out)
-    }
-}
-
-impl FromSql<Integer, Sqlite> for InternalPlayerId {
-    fn from_sql(bytes: <Sqlite as Backend>::RawValue<'_>) -> DeserResult<Self> {
-        let id = <InternalId as FromSql<Integer, Sqlite>>::from_sql(bytes)
-            .expect("column can be expressed as internal ID!");
-
-        Ok(InternalPlayerId(id))
-    }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, AsExpression, FromSqlRow)]
-#[diesel(sql_type = Integer)]
-pub struct InternalMatchId(InternalId);
-
-impl ToSql<Integer, Sqlite> for InternalMatchId {
-    fn to_sql<'b>(&'b self, out: &mut Output<'b, '_, Sqlite>) -> SerResult {
-        <InternalId as ToSql<Integer, Sqlite>>::to_sql(&self.0, out)
-    }
-}
-
-impl FromSql<Integer, Sqlite> for InternalMatchId {
-    fn from_sql(bytes: <Sqlite as Backend>::RawValue<'_>) -> DeserResult<Self> {
-        let id = <InternalId as FromSql<Integer, Sqlite>>::from_sql(bytes)
-            .expect("column can be expressed as internal ID!");
-
-        Ok(InternalMatchId(id))
-    }
-}
+integer_type!(InternalPlayerId);
+integer_type!(InternalMatchId);
